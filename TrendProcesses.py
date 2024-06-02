6c654d43f39c6c43df0cd2a4bd57631c3808e858
LiamTF <liamthaisen@gmail.com>
2024-06-03 10:57:21 +1200

    Update (6)

--- TrendProcesses.py
+++ TrendProcesses.py
@@ -13,8 +13,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, accuracy_score
 
 class FetchData:
 
